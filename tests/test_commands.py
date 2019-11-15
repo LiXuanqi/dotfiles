7e7e8f383fa4c1393a09ed662297e27937c2e21e
LiXuanqi <lixuanqi1995@gmail.com>
2019-11-14 18:08:32 -0800

    add tests to test commands exsits

---
+++ tests/test_commands.py
@@ -0,0 +1,10 @@
+import os
+
+def is_program_exist(program_name):
+    if os.system('command -v %s >/dev/null 2>&1' % program_name) != 0:
+        return False
+    return True
+
+def test_commands():
+    assert is_program_exist('ls')
+    
